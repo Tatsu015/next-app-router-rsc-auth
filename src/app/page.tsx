b2a257214cf2ed517d4c740230854c9a2cd3aed9
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-10-12 22:01:21 +0900

    add log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import SignOut from "@/components/signout";
 
 export default async function Home() {
   const session = await auth();
+  console.log("session");
   console.log(session);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
