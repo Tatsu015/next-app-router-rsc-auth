eaaeb99a6af185fadc9d25a0d36bd5488087b236
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-10-13 23:23:43 +0900

    refactor middleware

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import SignIn from "@/components/signin";
+import React from "react";
+
+function SignUpPage() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+}
+
+export default SignUpPage;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "./auth";
+
+export default auth((req) => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+
+    console.log('pathname', nextUrl.pathname);
+    console.log('isLoggedIn', isLoggedIn);
+});
+
+export const config = {
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
